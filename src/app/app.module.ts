03ef6640cdfcf9f6cc0ebd8940a8395a24266c8b
nodm <nodm@ciklum.com>
2017-05-08 10:08:18 +0300

    Animation added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from './shared/shared.module';
@@ -14,6 +15,7 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     SharedModule,
     CoreModule,
     AppRoutingModule,
